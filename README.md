9165303748ea06dca57af1f30fdc5df5bdc735f3
Aris Giotis <apjoti@gmail.com>
2023-08-20 20:36:02 +0300

    Update README.md & dna drill completed.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # js-drills
+
 JavaScript exercises to sharpen my skills regarding aspects such as string manipulation, arrays, etc.
+
+# useful tips
+
+When coding instead of running <code>node someFile.js</code> after saving changes, do the following.
+
+1 - Open terminal
+
+2 - <code>node --watch somefile.js</code>
+
+This way everytime you save changes in <code>someFile.js</code> node automatically runs the code. Check <a href="https://www.youtube.com/watch?v=QPSnFzj9eUw">here</a>.
---
+++ dna.js
@@ -0,0 +1,23 @@
+// write a function that computes the REVERSE COMPLEMENT of a dna sequence
+// A  with T
+// G with C
+
+function dnaReverseComplement(dnaSequense) {
+  return dnaSequense
+    .split("")
+    .reverse()
+    .map((elem) => {
+      if (elem === "A") {
+        return "T";
+      } else if (elem === "T") {
+        return "A";
+      } else if (elem === "G") {
+        return "C";
+      } else if (elem === "C") {
+        return "G";
+      }
+    })
+    .join("");
+}
+
+console.log(dnaReverseComplement("ATGC")); // GCAT
