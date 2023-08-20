9b3a15b456c8d1030aea9d5350fe79f955206a31
Aris Giotis <apjoti@gmail.com>
2023-08-20 22:41:21 +0300

    added steps to solution before implementations

--- shortest.cjs
+++ shortest.cjs
@@ -3,6 +3,14 @@
 
 // export default
 function shortestSubstring(inputString) {
+  // find all separate chars that comprise the string
+  // count them -> output minimum value
+  // use regex to track acceptable substrings that satisfy condition
+  // e.g. if chars are abcd -> /^[abcd]+$/
+  // which means that whichever is left to + must be present and not other chars
+  // now this is dynamic, we will use string interpolation
+  // track the shortest one
+  // return the length of the shortest one, less it can is the MINIMUM value
   return inputString;
 }
 
